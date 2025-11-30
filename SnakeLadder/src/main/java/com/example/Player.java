0a4af25618c1ca8b1ac6bcffb52164f97c6659e9
rohitkumarhem <rohitkumarhem@gmail.com>
2025-11-30 07:36:47 +0530

    first commit

---
+++ SnakeLadder/src/main/java/com/example/Player.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Player {
+    String id;
+    int currentPosition;
+
+    public Player(String id, int currentPosition) {
+        this.id = id;
+        this.currentPosition = currentPosition;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+}
